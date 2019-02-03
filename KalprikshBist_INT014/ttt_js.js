578c54842c5c0441ab89472b8788e5536921e442
kalpriksh <kalprikshbist@gmail.com>
2019-02-03 08:59:35 +0530

    modified JS and CSS

--- KalprikshBist_INT014/ttt_js.js
+++ KalprikshBist_INT014/ttt_js.js
@@ -4,21 +4,68 @@ var combi_odd ="";
 
 var chance=0;
 
-var combi_list={"123","456","789","147","258","369","159","357"};
-
 function myFunction(cellId){
 
-  myCell=document.getElementById("cellId");
+
+  console.log(cellId.innerHTML);
+
 
   if(chance%2==0)
   {
-    myCell.setAttribute(background-color,darkgrey);
-    myCell.innerHTML=o;
+    cellId.innerHTML="X";
+    cellId.style.backgroundColor='green';
+    var one = document.getElementById("1").innerHTML;
+    var two = document.getElementById("2").innerHTML;
+    var three = document.getElementById("3").innerHTML;
+    var four = document.getElementById("4").innerHTML;
+    var five = document.getElementById("5").innerHTML;
+    var six = document.getElementById("6").innerHTML;
+    var seven = document.getElementById("7").innerHTML;
+    var eight = document.getElementById("8").innerHTML;
+    var nine = document.getElementById("9").innerHTML;
+
+    if((one == two && two == three)||
+       (four == five && five== six)||
+       (seven == eight && eight == nine)||
+       (one == four && four == seven)||
+       (five == two && two == eight)||
+       (three == six && six == nine)||
+       (nine == five && five == one)||
+       (seven == five && five == three))
+       {
+         setTimeout(function(){
+         alert("X won");
+       },125);
+
+       }
   }
   else
   {
-    myCell.setAttribute(background-color,red);
-    myCell.innerHTML(X);
+    cellId.innerHTML="O";
+    cellId.style.backgroundColor='red';
+    var one = document.getElementById("1").innerHTML;
+    var two = document.getElementById("2").innerHTML;
+    var three = document.getElementById("3").innerHTML;
+    var four = document.getElementById("4").innerHTML;
+    var five = document.getElementById("5").innerHTML;
+    var six = document.getElementById("6").innerHTML;
+    var seven = document.getElementById("7").innerHTML;
+    var eight = document.getElementById("8").innerHTML;
+    var nine = document.getElementById("9").innerHTML;
+    if((one == two && two == three)||
+       (four == five && five == six)||
+       (seven == eight && eight == nine)||
+       (one == four && four == seven)||
+       (five == two && two == eight)||
+       (three == six && six == nine)||
+       (nine == five && five == one)||
+       (seven == five && five == three))
+       {
+         setTimeout(function(){
+           alert("O won");
+         },125);
+
+       }
   }
   chance++;
 }
